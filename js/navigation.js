3a0bff34a38613de21f83e4a29a188e4bd1e06a5
shelbgreenwell-lab <shelb.greenwell@gmail.com>
2026-08-28 20:38:12 -0600

    Create navigation.js

---
+++ js/navigation.js
@@ -0,0 +1,13 @@
+/*
+ * Operations Maturity System
+ * Navigation controller.
+ *
+ * Responsible for:
+ * - global navigation behavior
+ * - active navigation states
+ * - mobile navigation
+ * - page transitions
+ * - shared navigation interactions
+ *
+ * Keep feature-specific application logic out of this file.
+ */
